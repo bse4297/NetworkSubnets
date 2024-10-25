15075094ce318384dd74e44a0d3c67b287dcd72c
BuysChips <43148665+BuysChips@users.noreply.github.com>
2024-10-25 14:49:00 -0400

    Create subnet.py

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 26644
+
+#problem 1
+p1Network = '12.132.152.4'
+p1Broadcast = '12.132.152.7'
+p1First = '12.132.152.1'
+p1Last =  '12.132.152.6'
+
+#problem 2
+p2Network = '179.117.114.32'
+p2Broadcast = '179.117.114.63'
+p2First = '179.117.114.33'
+p2Last = '179.117.114.62'
+
+#problem 3
+p3Network = '90.102.75.0'
+p3Broadcast = '90.102.75.255'
+p3First = '90.102.75.1'
+p3Last = '90.102.75.254'
+
+#problem 4
+p4Network = '1.88.37.128'
+p4Broadcast = '1.88.37.143'
+p4First = '1.88.37.129'
+p4Last = '1.88.37.142'
+
+#problem 5
+p5Network = '168.73.255.128'
+p5Broadcast = '168.73.255.255'
+p5First = '168.73.255.129'
+p5Last = '168.73.255.254'
+
+#problem 6
+p6Network = '79.58.217.216'
+p6Broadcast = '79.58.217.223'
+p6First = '79.58.217.217'
+p6Last =  '79.58.217.222'
+
+#problem 7
+p7Network = '246.43.178.0'
+p7Broadcast = '246.43.178.63'
+p7First = '246.43.178.1'
+p7Last = '246.43.178.254'
+
+#problem 8
+p8Network = '157.28.140.0'
+p8Broadcast = '157.28.140.255'
+p8First = '157.28.140.1'
+p8Last = '157.28.140.254'
+
+#problem 9
+p9Network = '68.13.102.64'
+p9Broadcast = '68.13.102.95'
+p9First = '68.13.102.65'
+p9Last = '68.13.102.94'
+
+#problem 10
+p10Network = '235.254.64.128'
+p10Broadcast = '235.254.64.255'
+p10First = '235.254.64.129'
+p10Last = '235.254.64.254'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
